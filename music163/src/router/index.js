4c1f93b074233201805d704d69caadbae87d95e9
Scherger <984548954@qq.com>
2017-11-02 09:24:54 +0800

    the second stage

--- music163/src/router/index.js
+++ music163/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import header from '@/components/header'//头部
 import index from '@/components/index'//首页、推荐音乐
 import hot from '@/components/hot'//热歌榜
 import seach from '@/components/seach'//搜索
@@ -14,6 +13,17 @@ export default new Router({
       path: '/',
       name: 'index',
       component: index
-    }
+    },
+    {
+      path: '/hot',
+      name: 'hot',
+      component: hot
+    },
+    {
+      path: '/seach',
+      name: 'seach',
+      component: seach
+    }           
+
   ]
 })
