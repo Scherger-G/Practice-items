7cdeae05fe12db3d72ca2b6ed223d2c0d915f3d8
Scherger <984548954@qq.com>
2017-11-23 22:54:23 +0800

    更改颜色，封装方法

--- lottery/grids/js/main.js
+++ lottery/grids/js/main.js
@@ -1,136 +1,138 @@
-
-window.onload =function(){
+window.onload = function() {
 
 
     /**
- * 
- * 
- * @param {any} tagName 
- * @param {any} classname 
- * @returns 
- */
-function getClassName(tagName,classname){
-    if(document.getElementsByClassName){
-        return document.getElementsByClassName(classname);
-    }else{
-        var results =[];
-        var elems = document.getElementsByTagName('*');
-        for(var i=0;i<elems.length;i++){
-            if(elems[i].className.indexOf(classname)!= -1){
-                results[results.length] = elems[i];
+     * 
+     * 
+     * @param {any} tagName 
+     * @param {any} classname 
+     * @returns 
+     */
+    function getClassName(tagName, classname) {
+        if (document.getElementsByClassName) {
+            return document.getElementsByClassName(classname);
+        } else {
+            var results = [];
+            var elems = document.getElementsByTagName('*');
+            for (var i = 0; i < elems.length; i++) {
+                if (elems[i].className.indexOf(classname) != -1) {
+                    results[results.length] = elems[i];
+                }
             }
+            return results;
         }
-        return results;
     }
-}
 
 
 
-var li = document.getElementsByClassName('valueNum');
+    function forLi(check) {
+        for (let i in li) {
+
+            if (li.hasOwnProperty(i)) { //只输出私有属性
+
+                let classT = li[i].className;
+
+                let classN = classT.split(' ');
+
+                if (!check) {
+                    if (classN[classN.length - 1] == 'target') { //移除 target
+                        li[i].classList.remove('target');
+                    }
 
 
-var num = 0;//编号
-var times = Math.ceil(Math.random()*50+(Math.random()*3));//随机时间
+                    if (classN[1] == 'num' + num) { //添加 target 实现颜色切换
 
-var flag = true;
+                        let allClass = classT + ' target';
+                        li[i].setAttribute('class', allClass);
+
+                    }
+
+                } else {
+                    if (classN[classN.length - 1] == 'target') {
+                        console.log(li[i].innerHTML);
+                        flag = true;
+
+                        cut = Math.random() * 10 + 90;
+                        turns = Math.ceil(Math.random() * 3 + 1); //旋转次数
+                        speed = Math.floor((Math.random() * 10)) + 3; //速度
 
 
-var speed = Math.floor((Math.random()*10))+3;//速度
-var turns = Math.ceil(Math.random()*3+1);//旋转次数
-var cut = Math.random()*10+90;
+                        console.log(cut)
+
+                    }
+                }
 
 
-
-
-function startTurn (){
-
-    if(num==8){num =0;}//归零
-    
-        for(let i in li){
-            
-            if(li.hasOwnProperty(i)){//只输出私有属性
-
-              let classT = li[i].className;
-                
-              let classN = classT.split(' ');
-    
-    
-              if(classN[classN.length-1]=='target'){//移除 target
-                li[i].classList.remove('target');
-              }
-              
-          
-              if(classN[1]=='num'+num){//添加 target 实现颜色切换
-    
-                let allClass = classT+' target';
-                li[i].setAttribute('class',allClass);
-               
-              }
-             
             }
         }
-    
+
+
+    }
+
+
+
+    var li = document.getElementsByClassName('valueNum');
+
+
+    var num = 0; //编号
+    var times = Math.ceil(Math.random() * 50 + (Math.random() * 3)); //随机时间
+
+    var flag = true;
+
+
+    var speed = Math.floor((Math.random() * 10)) + 3; //速度
+    var turns = Math.ceil(Math.random() * 3 + 1); //旋转次数
+    var cut = Math.random() * 10 + 90;
+
+
+
+
+    function startTurn() {
+
+        if (num == 8) { num = 0; } //归零
+
+        forLi()
+
         num++;
-        times +=speed;
+        times += speed;
 
 
-        if(times>cut){
-            times =30;
+        if (times > cut) {
+            times = 30;
             speed -= 0.5;
             turns--;
 
-            if(turns ==1){//最后一圈降低速度
-                speed = 5;
+            if (turns == 1) { //最后一圈降低速度
+                speed = 1;
                 times = 500;
-                cut = 510;
+                cut = 503;
             }
         }
 
-       
-
-        if(turns ==0){
-
-            for(let f in li){
-                
-                if(li.hasOwnProperty(f)){//只输出私有属性
-                    let classT = li[f].className;
-                    
-                  let classN = classT.split(' ');
-                    
-                    
-                  if(classN[classN.length-1]=='target'){
-                    console.log(li[f].innerHTML);
-                    flag = true;
-
-                    cut = Math.random()*10+90;
-                    turns = Math.ceil(Math.random()*3+1);//旋转次数
-                    speed = Math.floor((Math.random()*10))+3;//速度
 
 
-                     console.log(cut)
-            
-                  }
-                }
-            }
+        if (turns == 0) {
 
-           
-        }else{
-            setTimeout(startTurn,times);
+            forLi(true)
+
+
+        } else {
+            setTimeout(startTurn, times);
         }
-   
-}
+
+    }
 
 
 
 
-var star = document.getElementsByClassName('star');
+    var star = document.getElementsByClassName('star');
 
 
-    document.onclick = function(e){
+    document.onclick = function(e) {
         var target = e.target || e.srcElement;
-        if(target.className=='star' & flag == true){
-            flag =false;
-            setTimeout(startTurn,times);
+        if (target.className == 'star' & flag == true) {
+            flag = false;
+            setTimeout(startTurn, times);
         }
     }
 
